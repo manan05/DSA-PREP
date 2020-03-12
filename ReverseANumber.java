f11e5363837fe0fe3e2398b1bfaa97d765aadb74
Manan Arora <31929934+manan05@users.noreply.github.com>
2020-03-12 17:43:15 +0530

    Update ReverseANumber.java

--- ReverseANumber.java
+++ ReverseANumber.java
@@ -1,20 +1,23 @@
-
-public class ReverseANumber {
+class Solution {
     public int reverse(int x) {
         int z = 0;
-        long m = 0;
-        while(x!=0){
-            z = x%10;
-            m = m*10 + z;
-            x = x/10;
+        int m = 0;
+        boolean isneg = false; // taking a boolean var to check if the number is negative or not
+        if(x<0){
+            x = x*-1; // making the number positive so that we only have to check once
+            isneg = true;
         }
-        if(m>Integer.MAX_VALUE || m<Integer.MIN_VALUE){
-            return 0;
+        while(x>0){
+            z = x%10; //finding remainder
+            if(m>Integer.MAX_VALUE/10){ // checking if the value is surpassing the int limit in the previous step
+               return 0; 
+            }
+            m = m*10 + z;   //making the new number
+            x = x/10;   //making the loop end
         }
-        else{
-            return (int)m;
+        if(isneg == true){ //if the number was initially negative making it negative again.
+            return -m;
         }
-        
-        
+        return m;
     }
 }
