056680629cf0359bdc45f70555942cf138af6064
Manan Arora <31929934+manan05@users.noreply.github.com>
2020-03-13 16:21:29 +0530

    Update and rename Reverse-String to Reverse-String.java

--- LeetCode/Reverse-String.java
+++ LeetCode/Reverse-String.java
@@ -2,7 +2,7 @@ class Solution {
     public void reverseString(char[] s) {
         int front = 0;  //idx that points to front of the array
         int back = s.length - 1; //idx that points to the back of the array
-        for(;front<(s.length/2);front++,back--){
+        for(;front<(s.length/2);front++,back--){ // till s.length/2 so that it doesn't make the array same as question
             char temp = s[front];
             s[front] = s[back];
             s[back] = temp;
