819d13aa67715a885dc6feb16fc312eca6007a14
Manan Arora <31929934+manan05@users.noreply.github.com>
2020-03-14 17:49:37 +0530

    Update twoSum.java

--- LeetCode/twoSum.java
+++ LeetCode/twoSum.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] arr = new int[2]; //creating a new array to save the output indices
         for(int i = 0; i<nums.length;i++){ 
-            for(int j = i+1;j<nums.length;j++){ //staring from the next element that is (i+1)
+            for(int j = i+1;j<nums.length;j++){ //starting from the next element that is (i+1)
                 if(nums[i]+nums[j] == target){
                     arr[0] = i;
                     arr[1] = j;
